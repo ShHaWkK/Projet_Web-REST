700d7ba776f95fb12032a0a607f8170d496045db
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2023-12-16 16:33:10 +0100

    Update index.php in service

--- api/index.php
+++ api/index.php
@@ -1,5 +1,5 @@
 <?php
-#include_once './Controller/TodoController.php';
+include_once './Service/globalFunctions.php';
 include_once './Repository/BDD.php';
 
 // Skipper les warnings, pour la production (vos exceptions devront être gérées proprement)
@@ -7,6 +7,7 @@ error_reporting(E_ERROR | E_PARSE);
 
 // le contenu renvoyé par le serveur sera du JSON
 header("Content-Type: application/json; charset=utf8");
+
 // Autorise les requêtes depuis localhost
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS,PATCH');
@@ -15,6 +16,7 @@ header('Access-Control-Allow-Headers: Content-Type, Authorization');
 // On récupère l'URI de la requête et on le découpe en fonction des / 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri ); // On obtient un tableau de la forme ['index.php', 'todos', '1']
+echo "$uri";
 
 // Si on a moins de 3 éléments dans l'URI, c'est que l'on est sur l'index de l'API
 if (sizeof($uri) < 3) {
@@ -42,8 +44,8 @@ function exit_with_content($content = null, $code = 200) {
 // Composant principal du controlleur: cette fonction agit comme un routeur en redirigeant les requêtes vers le bon controlleur
 function controller($uri) {
     switch($uri[2]) {
-        case 'todos':
-            todo_controller($uri);
+        case 'Service':
+            $controller = new GlobalController();//renvoie a l'utilisation de la class GlobalController fonction de redirection 
             break;
         default:
             // Si la ressource demandée n'existe pas, alors on renvoie une erreur 404
