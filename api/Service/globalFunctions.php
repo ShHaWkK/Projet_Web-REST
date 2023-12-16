573cdb8278f766017df7bb796f3bcd228aa2b43a
Spatulox <M78stormtrooper@laposte.net>
2023-12-16 15:16:44 +0100

    add checkMsg
    this commit is after "some fix condition" and idk why.
    We use this function in the previous functions

--- api/Service/globalFunctions.php
+++ api/Service/globalFunctions.php
@@ -9,6 +9,19 @@ function checkError($msg){
 
 # -------------------------------------------------------------- #
 
+function checkMsg($msg, $wordToSearch){
+
+	if (empty($msg) || empty($wordToSearch))
+	{
+		exit_with_message("ERROR : Plz enter a valid msg or a valid wordToSearg in the msg");
+	}
+
+	if (strpos($msg, $wordToSearch) !== false){
+		return true;
+	}
+
+	return false;
+}
 
 
 
