c7ad03b85b966aaf8b9c27cad878db7b445b1407
Spatulox <M78stormtrooper@laposte.net>
2023-12-16 12:51:26 +0100

    add selectDB function

--- api/Repository/BDD.php
+++ api/Repository/BDD.php
@@ -2,7 +2,7 @@
 
 include_once './Service/globalFunctions.php';
 
-function connectBDD(){
+function connectDB(){
 	try {
 	    $db = new PDO(
 	        'pgsql:host=restpastropapi-database-1;
@@ -24,6 +24,39 @@ function connectBDD(){
 }
 
 
+function selectDB($table, $colums){
+	// $colums must be like that : $columns = "idusers, role"
+
+	$db = connectDB();
+
+	$dbRequest = 'SELECT '. $colums .' FROM '. $table;
+
+	try{
+		$result = $db->prepare($dbRequest);
+		$result->execute();
+
+		$reponse = $result->fetchAll();
+		if ($reponse == false)
+		{
+
+			exit_with_message("ERROR : Impossible to select data");
+			//header('location: Accueil.php?message=' . $msg);
+			exit();
+		}
+		return $reponse;
+	}
+	catch (PDOException $e)
+	{
+		if (checkError($e->getMessage(), $wordToSearch = "Undefined column"))
+		{
+			exit_with_message(explode("does not exist", explode(":", $e->getMessage())[3])[0] . "does not exist");
+			exit();
+		}
+
+	    exit_with_message("PDO error :" . $e->getMessage());
+	}
+
+}
 
 
 
