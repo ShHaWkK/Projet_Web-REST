5c26a639ba503d696829bdac3de5ee90f62ae729
ShHaWkK <alexandreuzan9@gmail.com>
2023-12-17 18:12:54 +0100

    UserService.php

--- api/Models/userModel.php
+++ api/Models/userModel.php
@@ -1,20 +1,46 @@
 <?php
 
-# Cet objet sert à transporter les données d'une entité Music de facon standard et indépendante de http ou de la BDD.
+include_once '../Repository/BDD.php';
+
 class UserModel {
     public $id_users;
     public $role;
     public $apiKey;
-    
-    public
-    function __construct($id_users, $role, $apiKey = null)
-    {       
+
+    public function __construct($id_users = null, $role = null, $apiKey = null) {
         $this->id_users = $id_users;
         $this->role = $role;
         $this->apiKey = $apiKey;
     }
+
+    public function save() {
+        if ($this->id_users === null) {
+            $columnArray = ['role'];
+            $columnData = ["'{$this->role}'"]; 
+            insertDB('users', $columnArray, $columnData);
+        } else {
+            $columnArray = ['role'];
+            $columnData = ["'{$this->role}'"];
+            $condition = "id_users={$this->id_users}";
+            updateDB('users', $columnArray, $columnData, $condition);
+        }
+    }
+
+    public function delete() {
+        if ($this->id_users !== null) {
+            $condition = "id_users={$this->id_users}";
+            deleteDB('users', $condition);
+        }
+    }
+
+    public static function getAllUsers() {
+        return selectDB('users', '*');
+    }
+
+    public static function getUserById($id) {
+        $condition = "id_users={$id}";
+        return selectDB('users', '*', $condition);
+    }
 }
-
-
 
 ?>
--- api/Service/UserService.php
+++ api/Service/UserService.php
@@ -5,39 +5,67 @@ require_once 'UserRepository.php';
 class UserService {
     private $userRepository;
 
+    /*
+    * Construction du service 
+    */
+
     public function __construct(UserRepository $userRepository) {
         $this->userRepository = $UserRepository();
     }
 
+    /*
+     *  Récupère tous les utilisateurs
+    */
     public function getAllUsers() {
-        return $this->userRepository->findAll();
+        return $this->userRepository->getUsers();
     }
+
+    /*
+     *  Récupère un utilisateur par son id
+    */
 
     public function getUserById($id) {
-        return $this->userRepository->findById($id);
+        return $this->userRepository->getUser($id);
     }
 
-    public function createUser($role) {
-        // Validation 
-        return $this->userRepository->save(['role' => $role]);
+    /*
+     *  Créer un utilisateur
+    */
+
+    public function createUser($id_users, $role, $apiKey) {
+        $newUser = new UserModel($id_users, $role, $apiKey);
+        return $this->userRepository->createUser($newUser);
     }
+
+    /*
+     *  Met à jour un utilisateur
+    */
+
+    public function updateUser($id_users, $role, $apiKey) {
+        $newUser = new UserModel($id_users, $role, $apiKey);
+        return $this->userRepository->updateUser($newUser);
+    }
+
+    /*
+     *  Met à jour le role d'un utilisateur
+    */
 
     public function updateUserRole($id, $newRole) {
         // Validation 
         return $this->userRepository->updateRole($id, $newRole);
     }
 
+    /*
+     *  Supprime un utilisateur
+    */
     public function deleteUser($id) {
         return $this->userRepository->delete($id);
     }
 
-    public function createUser($role) {
-        if (!$this->isValidRole($role)) {
-            throw new Exception("Invalid role specified.");
-        }
-        return $this->userRepository->save(['role' => $role]);
-    }
-
+    /*
+     *  Vérifie si le role est valide
+    */
+    
     public function updateUserRole($id, $newRole) {
         if (!$this->isValidRole($newRole)) {
             throw new Exception("Invalid role specified.");
@@ -45,4 +73,52 @@ class UserService {
         return $this->userRepository->updateRole($id, $newRole);
     }
 
+    /*
+     *  Vérifie si la clé API est valide
+    */
+
+    public function isValidRole($role) {
+        return in_array($role, ['admin', 'user']);
+    }
+    /*
+    *  La clé API est un hash md5 de l'identifiant de l'utilisateur
+    */
+    public function isValidApiKey($apiKey) {
+        return preg_match('/^[a-zA-Z0-9]{32}$/', $apiKey);
+    }
 }
+
+/*
+<?php
+
+include_once './Repository/UserRepository.php';
+
+class UserService {
+    private $userRepository;
+
+    public function __construct() {
+        $this->userRepository = new UserRepository();
+    }
+
+    public function getAllUsers() {
+        return $this->userRepository->getUsers();
+    }
+
+    public function getUserById($id) {
+        return $this->userRepository->getUser($id);
+    }
+
+    public function createUser($id_users, $role, $apiKey) {
+        $newUser = new UserModel($id_users, $role, $apiKey);
+        return $this->userRepository->createUser($newUser);
+    }
+
+    public function deleteUser($id) {
+        $this->userRepository->deleteUser($id);
+    }
+
+    public function updateUser($id_users, $role, $apiKey) {
+        $newUser = new UserModel($id_users, $role, $apiKey);
+        return $this->userRepository->updateUser($newUser);
+    }
+*/
