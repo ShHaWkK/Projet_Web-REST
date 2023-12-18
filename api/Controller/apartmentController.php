a965e31c4021ddb32fd9a7405e076ffb0ad24f1e
Spatulox <M78stormtrooper@laposte.net>
2023-12-18 16:24:28 +0100

    Merge branch 'feature/test/includeApart' into feature/apartments

---
+++ api/Controller/apartmentController.php
@@ -0,0 +1,69 @@
+
+<?php
+include_once './Service/apartmentService.php';
+
+function apartmentController($uri) {
+    $apartementService = new ApartmentService($uri);
+
+    switch($_SERVER['REQUEST_METHOD']) {
+
+
+        // Retrieve apartments
+        case 'GET':
+
+            if($uri[3]){
+                exit_with_content($apartementService->getApartmentById($uri[3]));
+            }
+            else{
+                exit_with_content($apartementService->getAllApartments());
+            }
+            
+            break;
+
+
+        // Create apartment
+        case 'POST':
+            $jsonData = file_get_contents('php://input');
+            $data = json_decode($jsonData, true);
+
+            exit_with_content($apartementService->addApartment(null, $data['place'], $data['address'], $data['complement_address'], $data['availability'], $data['price_night'], $data['area'], $data['id_users']));
+            break;
+
+
+        // Update apartment
+        case 'PUT':
+            $jsonData = file_get_contents('php://input');
+            $data = json_decode($jsonData, true);
+
+            exit_with_content($apartementService->updateApartment($uri[3], $data['place'], $data['address'], $data['complement_address'], $data['availability'], $data['price_night'], $data['area']));
+            break;
+
+
+        // Update the availability of the apartment
+        case 'PATCH':
+
+            $jsonData = file_get_contents('php://input');
+            $data = json_decode($jsonData, true);
+
+            exit_with_content($apartementService->updateApartmentAvail($uri[3], $data["availability"]));
+            break;
+
+
+        // Delete the apartment
+        case 'DELETE':
+            if($uri[3]){
+                exit_with_content($apartementService->deleteApartment($uri[3]));
+            }
+            else{
+                exit_with_message("ERROR : Plz specifie the id of the apartment you want to delete.");
+            }
+            
+            break;
+
+
+        default:        
+            header("HTTP/1.1 200 OK");
+            exit();
+    }
+}
+?>
---
+++ api/Repository/apartmentRepository.php
@@ -0,0 +1,79 @@
+<?php
+include_once './Repository/BDD.php';
+include_once './Models/apartmentModel.php';
+include_once './exceptions.php';
+
+class ApartmentRepository {
+    private $connection = null;
+
+    // I'm not sure about this function lol (unuse)
+    function __construct() {
+        try {
+            $this->connection = pg_connect("host=restpastropapi-database-1 port=5432 dbname=apiDev_db user=apiDev password=password");
+            if (  $this->connection == null ) {
+                throw new BDDException("Could not connect to database.");
+            }
+        } catch (Exception $e) {
+            throw new BDDException("Could not connect db: ". $e->getMessage());
+        }
+    }
+    //-------------------------------------
+
+    public function getApartments(){
+        $apartArray = selectDB("APARTMENT", "*");
+
+        $apart = [];
+        $apartTest = [];
+
+        for ($i=0; $i < count($apartArray); $i++) { 
+            $apartTest[$i] = new ApartmentModel($apartArray[$i]['id_apartement'], $apartArray[$i]['place'], $apartArray[$i]['address'], $apartArray[$i]['complement_address'], $apartArray[$i]['availability'], $apartArray[$i]['price_night'], $apartArray[$i]['area'], $apartArray[$i]['id_users']);
+        }
+
+        return $apartTest;
+    }
+
+    public function getApartment($id){
+
+        $apart = selectDB("APARTMENT", "*", "id_apartement=".$id);
+
+        return new ApartmentModel($apart[0]['id_apartement'], $apart[0]['place'], $apart[0]['address'], $apart[0]['complement_address'], $apart[0]['availability'], $apart[0]['price_night'], $apart[0]['area'], $apart[0]['id_users']);
+    }
+
+    public function deleteApartment($id){
+        if(deleteDB("APARTMENT", "id_apartement=".$id)){
+            exit_with_message("Deleted successful");
+        }
+        exit_with_message("Failed to delete");
+    }
+
+    
+    public function addApartment(ApartmentModel $apartment){
+        insertDB("APARTMENT", ["place", "address", "complement_address", "availability", "price_night", "area", "id_users"], [$apartment->place, $apartment->address, $apartment->complement_address, $apartment->availability, $apartment->price_night, $apartment->area, $apartment->id_users]);
+
+        //SELECT * FROM APARTMENT WHERE id_apartment = (SELECT MAX(id_apartment) FROM APARTMENT WHERE id_users = 'your_user_id');
+        $maxID = selectDB('APARTMENT', 'MAX("id_apartement")', "id_users=".$apartment->id_users)[0]['max'];
+        return selectDB('APARTMENT', '*', "id_apartement=".$maxID);
+    }
+
+    
+    public function updateApartment($id_apartement, $colunm, $values){
+
+        // var_dump($colunm);
+        // var_dump($values);
+        //exit();
+        
+        
+        if (updateDB("APARTMENT", $colunm, $values, "id_apartement=".$id_apartement)){
+
+            return true;
+            //exit_with_message("Updated successful");
+        }
+        exit_with_message("Updated failed");
+        
+
+        
+    }
+}
+
+
+?>
